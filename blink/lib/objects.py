1de28e30dfb4fb6e9a6d51e43f11600c56bbcd12
Jordan Miller <jordan.kay@gmail.com>
2020-10-01 23:03:52 -0600

    added visualization and name

--- blink/lib/objects.py
+++ blink/lib/objects.py
@@ -1,7 +1,8 @@
 class Function():
     ''' basically a function that is responsible for getting its own inputs '''
 
-    def __init__(self, inputs: dict = None):
+    def __init__(self, inputs: dict = None, name: str = None):
+        self.name = name or self.__repr__().split()[0].split('.')[-1]
         self.set_inputs(inputs=inputs or {})
         self.clear(memory=True)
 
@@ -47,6 +48,41 @@ class Function():
 
     def get(self):
         return self.output
+
+    def visualize(self, size=(8,5)):
+        '''
+        minimal, no uniformly layout, unable to save to file, no namespacing
+        and no indications such as cached, cost in time, or popularity, etc.
+        '''
+
+        def graph_heritage(current, seen):
+            seen.append(current)
+            parents = [v for v in current.inputs.values()]
+            for parent in parents:
+                if not graph.has_node(parent.name):
+                    graph.add_node(parent.name)
+                    if parent in [v for v in self.inputs.values()]:
+                        colors.append('#d7a9e3')
+                    else:
+                        colors.append('#8bbee8')
+                ancestors.append((parent.name, current.name))
+                if parent not in seen:
+                    graph_heritage(current=parent, seen=seen)
+
+        import networkx as nx
+        import matplotlib.pyplot as plt
+        graph = nx.DiGraph()
+        colors = []
+        ancestors = []
+        if not graph.has_node(self.name):
+            graph.add_node(self.name)
+            colors.append('#a8d5ba')
+        graph_heritage(current=self, seen=[])
+        graph.add_edges_from(ancestors, weight=1)
+        pos = nx.spring_layout(graph)
+        nx.draw(graph, pos, with_labels=True, node_color=colors, node_size=1500)
+        plt.rcParams["figure.figsize"] = size
+        plt.show()
 
     def function(self):
         ''' main '''
